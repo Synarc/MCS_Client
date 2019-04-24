23aa4dbfd4e33510200b15054e801eb9f83a2add
synarc1 <isaacsilas05@gmail.com>
2019-04-24 11:27:04 +1000

    Initial commit

---
+++ app/src/main/java/com/mcssoftware/app/mcsclient/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mcssoftware.app.mcsclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
